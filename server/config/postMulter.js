77d994872aec87c3150646d918cc446ea98fd4f7
codewools <201400044@gift.edu.pk>
2024-05-22 21:44:44 +0500

    Change the name of the file that has to be stored on the database

--- server/config/postMulter.js
+++ server/config/postMulter.js
@@ -96,7 +96,13 @@ const storage = multer.diskStorage({
         }
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname)); // appending extension
+        const timestamp = Date.now();
+        const originalName = path.parse(file.originalname).name; // extract the original filename without extension
+        const extension = path.extname(file.originalname); // extract the file extension
+        const unique = "Q-D-H-T-E";
+        const randomNumber = Math.floor(Math.random() * 10000); // generate a random number between 0 and 9999
+        const newFilename = `${originalName}-${unique}-${timestamp}-${randomNumber}${extension}`; // concatenate original name, unique string, timestamp, random number, and extension
+        cb(null, newFilename);
     }
 });
 
